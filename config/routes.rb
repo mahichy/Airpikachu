37bf1a7ce0344525e3a86c5c702c0add100b24ce
mahichy <43366576+mahichy@users.noreply.github.com>
2019-08-23 22:51:44 +0600

    Create user profile page
    with the problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  root 'pages#home'
 
 
   devise_for :users,
-  path: '',
-  path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+              path: '',
+              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
+  
+
+  resources :users, only: [:show]
  
 end
