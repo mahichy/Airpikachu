9399e01aa30b946ab76b507a6123953efede141a
mahichy <43366576+mahichy@users.noreply.github.com>
2019-09-28 20:05:21 +0600

    Creating Show Reviews Page
    Task_12_Class_05

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
         # Display all the guest reviews to host (if this user is a host)
         @guest_reviews = Review.where(type: "GuestReview", host_id: @user.id)
 
-        # Display all the host reviews to guest (if this user is a guest)
+        # Display all the host reviews to host (if this user is a guest)
         @host_reviews = Review.where(type: "HostReview", guest_id: @user.id)
     end
 end
